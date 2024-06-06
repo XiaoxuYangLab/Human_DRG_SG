1af76ee10f1c8e6e348f36c855d11967f584d01e
Xiaoxu Yang <yangxx@mail.cbi.pku.edu.cn>
2024-06-06 12:18:33 -0700

    Update Start_population_simulation.R

--- Analysis/Simulate_starting_population/Start_population_simulation.R
+++ Analysis/Simulate_starting_population/Start_population_simulation.R
@@ -30,7 +30,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2))&input$flag[i]==1){
@@ -74,7 +73,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2))&input$flag[i]==1){
@@ -118,7 +116,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2))&input$flag[i]==1){
@@ -162,7 +159,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2))&input$flag[i]==1){
@@ -207,7 +203,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2))&input$flag[i]==1){
@@ -251,7 +246,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2))&input$flag[i]==1){
@@ -295,7 +289,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2))&input$flag[i]==1){
@@ -339,7 +332,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2))&input$flag[i]==1){
@@ -384,7 +376,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_C[i]*2,input$SG_AVG_C[i]*2))&input$flag[i]==1){
@@ -428,7 +419,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_DRG_SG_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$DRG_AVG_T[i]*2,input$SG_AVG_T[i]*2))&input$flag[i]==1){
@@ -473,7 +463,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_C[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_C[i]*2,input$RIGHT_AVG_C[i]*2))&input$flag[i]==1){
@@ -517,7 +506,6 @@ raw_input$hyperUB <- NA
 input$flag[i]=1
 for(j in 1:1000){
 	temp<-hyperCI(2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2),raw_input$idd[j],ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2))
-#	raw_input$hyperLB[j] <- temp[ceiling(nrow(temp)/2),1]/raw_input$idd[j]
 	raw_input$hyperLB[j] <- max(0,2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[2])
 	raw_input$hyperUB[j] <- 2*ceiling(raw_input$idd[j]*input$MEAN_LEFT_RIGHT_T[i]*2)/temp[1]
 	if(!(raw_input$hyperLB[j]>min(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2)&raw_input$hyperUB[j]<max(input$LEFT_AVG_T[i]*2,input$RIGHT_AVG_T[i]*2))&input$flag[i]==1){
@@ -541,151 +529,6 @@ print(plot)
 dev.off()
 
 write.csv(input,file="8990_both_L_R_T.csv")
-
-## summary of all the simulations
-library(ggplot2)
-raw<-read.csv(file="Hypergeometric_estimation.csv",header=T)
-names(raw)
-[1] "ID"               "Type"             "Estimated_number"
-[4] "CHR_POS_REF_ALT"  "Variant_LR"       "Variant_DRGSG" 
-
-pdf(file="Estimated_number_of_common_ancestors_shared_variants.pdf",width=12,height=6)
-ggplot(raw,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-#	geom_boxplot(notch=TRUE)+
-	geom_violin()+
-	geom_jitter(alpha=0.5,aes(col=Variant_DRGSG))+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-
-ggplot(raw,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-	geom_boxplot(notch=TRUE)+
-#	geom_violin()+
-	geom_jitter(alpha=0.5,aes(col=Variant_DRGSG))+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-dev.off()
-
-
-pdf(file="Estimated_number_of_common_ancestors_shared_variants_BW.pdf",width=12,height=6)
-ggplot(raw,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-#	geom_boxplot(notch=TRUE)+
-	geom_violin()+
-	geom_jitter(alpha=0.5)+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-
-ggplot(raw,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-	geom_boxplot(notch=TRUE)+
-#	geom_violin()+
-	geom_jitter(alpha=0.5)+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-dev.off()
-
-#for variants are shared by both Left and Right ( 0.6666667<correct left/Right<1.5)
-
-raw2<-raw[raw$Variant_LR=="Both_LR",]
-
-
-pdf(file="Estimated_number_of_common_ancestors_only_LR_both_variants.pdf",width=12,height=6)
-ggplot(raw2,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-#	geom_boxplot(notch=T)+
-	geom_violin()+
-	geom_jitter(alpha=0.8,aes(col=raw2$Variant_DRGSG))+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-	
-ggplot(raw2,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-	geom_boxplot(notch=T)+
-#	geom_violin()+
-	geom_jitter(alpha=0.8,aes(col=raw2$Variant_DRGSG))+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-dev.off()
-
-pdf(file="Estimated_number_of_common_ancestors_only_LR_both_variants_BW.pdf",width=12,height=6)
-ggplot(raw2,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-#	geom_boxplot(notch=T)+
-	geom_violin()+
-	geom_jitter(alpha=0.8)+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-	
-ggplot(raw2,aes(x=factor(Type,level=c("DRG_SG_C","L_R_C","DRG_SG_T","L_R_T")),y=log10(Estimated_number)))+
-	geom_boxplot(notch=T)+
-#	geom_violin()+
-	geom_jitter(alpha=0.8)+
-	facet_wrap(~ID,strip.position = "bottom")+
-	theme_classic()
-dev.off()	
-
-##statistics for all variants that are shared by both Left and Right ( 0.6666667<correct left/Right<1.5)
-
-# ID06 C 
-wilcox.test(raw2[raw2$ID=="8553"&raw2$Type=="DRG_SG_C",]$Estimated_number,raw2[raw2$ID=="8553"&raw2$Type=="L_R_C",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
-# data:  raw2[raw2$ID == "8553" & raw2$Type == "DRG_SG_C", ]$Estimated_number and raw2[raw2$ID == "8553" & raw2$Type == "L_R_C", ]$Estimated_number
-# W = 260.5, p-value = 0.03589
-# alternative hypothesis: true location shift is not equal to 0
-
-# Warning message:
-# In wilcox.test.default(raw2[raw2$ID == "8553" & raw2$Type == "DRG_SG_C",  :
-#  cannot compute exact p-value with ties
-
-
-
-# ID06 T
-wilcox.test(raw2[raw2$ID=="8553"&raw2$Type=="DRG_SG_T",]$Estimated_number,raw2[raw2$ID=="8553"&raw2$Type=="L_R_T",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
-# data:  raw2[raw2$ID == "8553" & raw2$Type == "DRG_SG_T", ]$Estimated_number and raw2[raw2$ID == "8553" & raw2$Type == "L_R_T", ]$Estimated_number
-# W = 436, p-value = 0.4072
-# alternative hypothesis: true location shift is not equal to 0
-
-# Warning message:
-# In wilcox.test.default(raw2[raw2$ID == "8553" & raw2$Type == "DRG_SG_T",  :
-#  cannot compute exact p-value with ties
-
-
-# ID07 C 
-wilcox.test(raw2[raw2$ID=="8718"&raw2$Type=="DRG_SG_C",]$Estimated_number,raw2[raw2$ID=="8718"&raw2$Type=="L_R_C",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
-# data:  raw2[raw2$ID == "8718" & raw2$Type == "DRG_SG_C", ]$Estimated_number and raw2[raw2$ID == "8718" & raw2$Type == "L_R_C", ]$Estimated_number
-# W = 485, p-value = 4.019e-12
-# alternative hypothesis: true location shift is not equal to 0
-
-
-# ID07 T
-wilcox.test(raw2[raw2$ID=="8718"&raw2$Type=="DRG_SG_T",]$Estimated_number,raw2[raw2$ID=="8718"&raw2$Type=="L_R_T",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
-# data:  raw2[raw2$ID == "8718" & raw2$Type == "DRG_SG_T", ]$Estimated_number and raw2[raw2$ID == "8718" & raw2$Type == "L_R_T", ]$Estimated_number
-# W = 645.5, p-value = 1.448e-09
-# alternative hypothesis: true location shift is not equal to 0
-
-
-# ID08 C
-wilcox.test(raw2[raw2$ID=="8990"&raw2$Type=="DRG_SG_C",]$Estimated_number,raw2[raw2$ID=="8990"&raw2$Type=="L_R_C",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
-# data:  raw2[raw2$ID == "8990" & raw2$Type == "DRG_SG_C", ]$Estimated_number and raw2[raw2$ID == "8990" & raw2$Type == "L_R_C", ]$Estimated_number
-# W = 16510, p-value < 2.2e-16
-# alternative hypothesis: true location shift is not equal to 0
-
-
-
-# ID08 T
-wilcox.test(raw2[raw2$ID=="8990"&raw2$Type=="DRG_SG_T",]$Estimated_number,raw2[raw2$ID=="8990"&raw2$Type=="L_R_T",]$Estimated_number,alternative="two.sided")
-
-# Wilcoxon rank sum test with continuity correction
-
 # data:  raw2[raw2$ID == "8990" & raw2$Type == "DRG_SG_T", ]$Estimated_number and raw2[raw2$ID == "8990" & raw2$Type == "L_R_T", ]$Estimated_number
 # W = 9367, p-value < 2.2e-16
 # alternative hypothesis: true location shift is not equal to 0
